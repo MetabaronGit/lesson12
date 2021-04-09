2cf880f18ec57b93d1fe4d635fe739983c7f3022
rad <cepelka.jr@email.cz>
2021-04-09 11:57:04 +0200

    09-01

--- scraping_mk2.py
+++ scraping_mk2.py
@@ -1,1 +1,11 @@
 # pokusy se scrapingem
+import requests
+import json
+
+try:
+    content = requests.get("http://example.com")
+except Exception:
+    print("chyba!")
+finally:
+    print("status code:", content.status_code)
+    print(content.text)
