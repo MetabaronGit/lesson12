3431f9c8a34922173d68cd1f42bb07be067615a8
rad <cepelka.jr@email.cz>
2021-04-09 15:31:08 +0200

    09-02

--- scraping_mk2.py
+++ scraping_mk2.py
@@ -1,11 +1,22 @@
 # pokusy se scrapingem
 import requests
-import json
+import bs4
+
+# odfiltrování prázdných řádků
+def clear_content(content: list):
+    for item in content:
+        if item == "\n":
+            content.remove(item)
 
 try:
     content = requests.get("http://example.com")
+    print("status code:", content.status_code)
+    soup = bs4.BeautifulSoup(content.text, "html.parser")
+    # print(soup)
 except Exception:
     print("chyba!")
-finally:
-    print("status code:", content.status_code)
-    print(content.text)
+
+text = soup.body.div.contents
+print(len(text))
+clear_content(text)
+print(len(text))
